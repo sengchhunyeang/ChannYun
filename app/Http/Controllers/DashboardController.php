d24d21d0f01b4e1b6ec7bdd4d303a2490d7f94fc
chhunyeangseng <chhunyeangseng@gmail.com>
2025-01-02 11:01:36 +0700

    firstcommit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class DashboardController extends Controller
+{
+    //
+    public function index($section = 'content-a')
+    {
+        // Default to 'content-a' if no section is provided
+        return view('dashboard', ['content' => $section]);
+    }
+
+}
